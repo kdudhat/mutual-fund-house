e2efbcd7ea98d36b01acc230a2655c11370c961b
kdudaht <kishandudhat412>
2021-10-09 13:57:21 +0530

    routing setup for different page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import * as ROUTES from "./constant/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/auth";
+// import { useAuthListener } from "./hooks/";
+import { Home, SignIn, SignUp, Profile } from "./pages";
+export default function App() {
+  // const { user } = useAuthListener();
+  const user = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.HOME}
+        path={ROUTES.SIGN_IN}
+      >
+        <SignIn />
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.HOME}
+        path={ROUTES.SIGN_UP}
+      >
+        <SignUp />
+      </IsUserRedirect>
+
+      <ProtectedRoute user={user} path={ROUTES.HOME} exact>
+        <Home />
+      </ProtectedRoute>
+      <ProtectedRoute user={user} path={ROUTES.PROFILE} exact>
+        <Profile />
+      </ProtectedRoute>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/helpers/auth.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import * as ROUTES from "../constant/routes";
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
+
+export function ProtectedRoute({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: ROUTES.SIGN_IN,
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
